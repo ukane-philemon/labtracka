f14b66e9aaa97a5e0dc79baa0a6f453b53f02bef
praise-cloud <georgewillpraise15@gmail.com>
2024-06-25 22:54:55 +0100

    i made style changes to the pagination

--- admin/src/components/FooterPagination.tsx
+++ admin/src/components/FooterPagination.tsx
@@ -1,18 +1,49 @@
-// import React from 'react'
-import { Box } from '@mui/material'
-import CustomSeparator from './CustomSeperator'
-import TablePaginationDemo from './TablePaginationDemo'
+"use client"
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Pagination from '@mui/material/Pagination';
+import PaginationItem from '@mui/material/PaginationItem';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
-const FooterPagination = () => {
+export default function FooterPagination() {
+  const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    // Handle the page change logic here
+    console.log(`Page: ${value}`);
+  };
+
   return (
-        <Box 
-        display='flex'
-        alignItems='center'>
-        <TablePaginationDemo />
-        <CustomSeparator />
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '16px',
+        borderTop: '1px solid #e0e0e0',
+        backgroundColor: '#f9f9f9',
+        marginTop: '20px',
+        // width: '996px',
+        height: '41px',
+      }}
+    >
+      <Typography variant="body2" color="textSecondary" sx={{fontFamily: 'Open Sans', fontSize: '18px', fontWeight: '400'}}>
+        Page 1 of 1
+      </Typography>
+      <Pagination
+        count={10}
+        renderItem={(item) => (
+          <PaginationItem
+            components={{ previous: NavigateBeforeIcon, next: NavigateNextIcon }}
+            {...item}
+          />
+        )}
+        onChange={handlePageChange}
+      />
     </Box>
-  )
+  );
 }
 
-export default FooterPagination
 
+
+ 
--- admin/src/components/declaration.d.ts
+++
@@ -1,7 +0,0 @@
-// declaration.d.ts
-declare module '*.svg' {
-  import * as React from 'react';
-  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  const src: string;
-  export default src;
-}
